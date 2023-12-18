67b71014abdd549270df57a3ade6bc751644d6c5
bahricanyesil <bahricanyesil@gmail.com>
2023-12-19 00:51:47 +0300

    Create the sample app fully remote with both logic and UI

---
+++ app/lib/network.dart
@@ -0,0 +1,118 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_js/flutter_js.dart';
+import 'package:http/http.dart' as http;
+import 'package:rfw/rfw.dart';
+
+import 'rfw/core.dart' as c;
+import 'rfw/material.dart' as m;
+
+class NetworkExample extends StatefulWidget {
+  const NetworkExample({super.key});
+
+  @override
+  State<NetworkExample> createState() => _NetworkExampleState();
+}
+
+class _NetworkExampleState extends State<NetworkExample> {
+  final _runtime = Runtime();
+  final _data = DynamicContent();
+  final _logic = getJavascriptRuntime();
+  bool loaded = false;
+  static const remoteName = LibraryName(['remote']);
+
+  @override
+  void initState() {
+    super.initState();
+    _runtime.update(
+      const LibraryName(['widgets']),
+      c.createCoreWidgets(),
+    );
+    _runtime.update(
+      const LibraryName(['material']),
+      m.createMaterialWidgets(),
+    );
+    _update();
+  }
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    _update();
+  }
+
+  void _update() async {
+    const url = 'http://localhost:8080';
+    final results = await Future.wait([
+      http.get(Uri.parse('$url/counter.js')),
+      http.get(Uri.parse('$url/counter.rfw')),
+    ]);
+    final logicRes = results[0];
+    final uiRes = results[1];
+    if (logicRes.statusCode != 200 || uiRes.statusCode != 200) {
+      return;
+    }
+    _logic.evaluate(logicRes.body, sourceUrl: 'script.js');
+    $state();
+    _runtime.update(remoteName, decodeLibraryBlob(uiRes.bodyBytes));
+    if (mounted) setState(() => loaded = true);
+  }
+
+  void $state() {
+    final local = _logic.jsonStringify(_logic.evaluate('state'));
+    _data.update('state', jsonDecode(local));
+  }
+
+  void onEvent(String name, DynamicMap arguments) async {
+    debugPrint('event $name($arguments)');
+    _logic.evaluate('$name()');
+    $state();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!loaded) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    final colors = Theme.of(context).colorScheme;
+    _data.update('colorScheme', <String, Object>{
+      'inversePrimary': colors.inversePrimary.value,
+      'inverseSurface': colors.inverseSurface.value,
+      'onInverseSurface': colors.onInverseSurface.value,
+      'primary': colors.primary.value,
+      'onPrimary': colors.onPrimary.value,
+      'primaryContainer': colors.primaryContainer.value,
+      'onPrimaryContainer': colors.onPrimaryContainer.value,
+      'secondary': colors.secondary.value,
+      'onSecondary': colors.onSecondary.value,
+      'secondaryContainer': colors.secondaryContainer.value,
+      'onSecondaryContainer': colors.onSecondaryContainer.value,
+      'tertiary': colors.tertiary.value,
+      'onTertiary': colors.onTertiary.value,
+      'tertiaryContainer': colors.tertiaryContainer.value,
+      'onTertiaryContainer': colors.onTertiaryContainer.value,
+      'error': colors.error.value,
+      'onError': colors.onError.value,
+      'errorContainer': colors.errorContainer.value,
+      'onErrorContainer': colors.onErrorContainer.value,
+      'background': colors.background.value,
+      'onBackground': colors.onBackground.value,
+      'surface': colors.surface.value,
+      'onSurface': colors.onSurface.value,
+      'outline': colors.outline.value,
+      'outlineVariant': colors.outlineVariant.value,
+      'scrim': colors.scrim.value,
+      'shadow': colors.shadow.value,
+    });
+    return Container(
+      color: colors.background,
+      child: RemoteWidget(
+        runtime: _runtime,
+        data: _data,
+        widget: const FullyQualifiedWidgetName(remoteName, 'root'),
+        onEvent: onEvent,
+      ),
+    );
+  }
+}
---
+++ server/.dart_frog/server.dart
@@ -0,0 +1,38 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, implicit_dynamic_list_literal
+
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+
+
+import '../routes/index.dart' as index;
+import '../routes/counter.rfw.dart' as counter_rfw;
+import '../routes/counter.js.dart' as counter_js;
+
+
+void main() async {
+  final address = InternetAddress.tryParse('') ?? InternetAddress.anyIPv6;
+  final port = int.tryParse(Platform.environment['PORT'] ?? '8080') ?? 8080;
+  hotReload(() => createServer(address, port));
+}
+
+Future<HttpServer> createServer(InternetAddress address, int port) {
+  final handler = Cascade().add(buildRootHandler()).handler;
+  return serve(handler, address, port);
+}
+
+Handler buildRootHandler() {
+  final pipeline = const Pipeline();
+  final router = Router()
+    ..mount('/', (context) => buildHandler()(context));
+  return pipeline.addHandler(router);
+}
+
+Handler buildHandler() {
+  final pipeline = const Pipeline();
+  final router = Router()
+    ..all('/', (context) => index.onRequest(context,))..all('/counter.rfw', (context) => counter_rfw.onRequest(context,))..all('/counter.js', (context) => counter_js.onRequest(context,));
+  return pipeline.addHandler(router);
+}
+
